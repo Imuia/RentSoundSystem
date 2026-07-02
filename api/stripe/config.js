a7f0f1c17396209f477a8972b03d78ee2d9400f7
Rakoto Anjaramamy <rakotoanjaramamy@gmail.com>
2026-07-02 16:01:43 +0300

    modif

--- api/stripe/config.js
+++ api/stripe/config.js
@@ -1,6 +1,7 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY ? "OK" : "MANQUANTE",
-    secretKey: process.env.STRIPE_SECRET_KEY ? "OK" : "MANQUANTE"
-  });
+export default function handler(req,res){
+
+res.status(200).json({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
+});
+
 }
